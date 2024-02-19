f619294d0451e85a632f5e9bdf3ce73ff54571f6
BL4CK4N0N <74794050+BL4ACK4N0N@users.noreply.github.com>
2024-02-19 21:14:18 +0100

    Delete tst.php

--- tst.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *"); // Replace * with the specific domain you want to allow, or use '*' for any domain.
-header("Content-Type: text/plain");
-
-// Your code to read and output the domain list goes here
-$fileContent = file_get_contents("domains.txt");
-echo $fileContent;
-?>
